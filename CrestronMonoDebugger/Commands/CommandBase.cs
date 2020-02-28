1cecff96d5bc8c716efa670bd2902e212196be7d
Gary Greendale <Gary@thegreendales.com>
2020-02-28 16:20:28 -0600

    Initial commit

---
+++ CrestronMonoDebugger/Commands/CommandBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrestronMonoDebugger.Commands
+{
+    internal class CommandBase
+    {
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        protected readonly Guid CommandSet = new Guid("2a1630cf-d13c-496f-b736-876beb6b9bfb");
+    }
+}
---
+++ CrestronMonoDebugger/Commands/DebugCommand.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace CrestronMonoDebugger.Commands
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class DebugCommand : CommandBase
+    {
+        #region Properties, Fields and Constants
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static DebugCommand Instance { get; private set; }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private AsyncPackage Package { get; set; }
+
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        private const int CommandId = 0x1120;
+
+        #endregion
+
+        #region Initialization
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DebugCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private DebugCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            Package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            Initialize(commandService);
+        }
+
+        private void Initialize(OleMenuCommandService commandService)
+        {
+            var menuCommandId = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(Execute, menuCommandId);
+            commandService.AddCommand(menuItem);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in DebugCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new DebugCommand(package, commandService);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "DebugCommand";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                Package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+
+        #endregion
+    }
+}
